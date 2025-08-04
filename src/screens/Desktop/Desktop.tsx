9a95e4d1fa9cd313b5af784a21325781d8031166
lilian79511 <46732651+lilian79511@users.noreply.github.com>
2025-08-04 22:00:08 +1200

    Make footer extend full width

--- src/screens/Desktop/Desktop.tsx
+++ src/screens/Desktop/Desktop.tsx
@@ -5,8 +5,10 @@ import { ProjectsSection } from "./sections/ProjectsSection/ProjectsSection";
 export const Desktop = (): JSX.Element => {
   return (
     <div className="w-full bg-[#F5F3EE] min-h-screen">
-      <div className="flex flex-col w-full max-w-[1440px] mx-auto">
+      <div className="w-full max-w-[1440px] mx-auto">
         <ProjectsSection />
+      </div>
+      <div className="w-full">
         <FooterSection />
       </div>
     </div>
